d1c0db4ad913192295e1fe715ac620022492a94d
Maheshwari123325 <maheshwari.ls123@gmail.com>
2025-10-22 20:50:57 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "WhatsApp Bot is running!"
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+
+    if 'hi' in incoming_msg:
+        msg.body("Hello! 👋 I'm your WhatsApp ordering bot. How can I help you today?")
+    else:
+        msg.body("Please say 'hi' to start chatting!")
+
+    return str(resp)
+
+if _name_ == '_main_':
+    app.run()
