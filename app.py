839fe0b1505182b016ba114bc2a3a92afe784721
Maheshwari123325 <maheshwari.ls123@gmail.com>
2025-10-25 09:55:54 +0530

    Make app.py compatible with Render by using dynamic PORT

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from main import app
 import os
 
-if _name_ == "_main_":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
