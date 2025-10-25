9bf6fad8546ed187284ab34a1078cbebc215daa4
Maheshwari123325 <maheshwari.ls123@gmail.com>
2025-10-25 09:47:48 +0530

    Make app.py compatible with Render by using dynamic PORT
    Updated Flask app to use environment variable PORT instead of fixed port number for deployment flexibility on Render and other platforms.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from main import app
+import os
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000)
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
