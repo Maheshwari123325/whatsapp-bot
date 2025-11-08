09fd6aac73d4cd327c43d35c3c232ab74eee8109
Maheshwari123325 <maheshwari.ls123@gmail.com>
2025-11-08 15:54:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,6 +60,6 @@ def home():
     return "OilWhatsAppBot is running successfully!"
 
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
